b08f3ea5393eb629cfd27b08260da6731bb636a9
zelp43 <zelp43@gmail.com>
2026-06-21 13:46:42 -0400

    Update downloads.js

--- downloads.js
+++ downloads.js
@@ -1,5 +1,5 @@
 const DOWNLOADS = {
-  mac_silicon: 'https://drive.google.com/drive/folders/1K-30KjHQ7hovRXwuUjGQw_Ka_DGnn8r8?usp=sharing',
-  mac_intel:   'https://drive.google.com/drive/folders/1K-30KjHQ7hovRXwuUjGQw_Ka_DGnn8r8?usp=sharing',
+  mac_silicon: 'https://pub-fb3edae0830742f69b9a6233c43fdb62.r2.dev/Selecta%20Tool-2.0.1-Silicon.dmg',
+  mac_intel:   'https://pub-fb3edae0830742f69b9a6233c43fdb62.r2.dev/Selecta%20Tool-2.0.1-Intel.dmg',
   windows:     'https://drive.google.com/drive/folders/1K-30KjHQ7hovRXwuUjGQw_Ka_DGnn8r8?usp=sharing',
 };
