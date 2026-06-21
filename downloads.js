2981c835341b3e8264ea4b80f479cb41b0076b69
zelp43 <zelp43@gmail.com>
2026-06-21 13:44:57 -0400

    Add files via upload

---
+++ downloads.js
@@ -0,0 +1,5 @@
+const DOWNLOADS = {
+  mac_silicon: 'https://drive.google.com/drive/folders/1K-30KjHQ7hovRXwuUjGQw_Ka_DGnn8r8?usp=sharing',
+  mac_intel:   'https://drive.google.com/drive/folders/1K-30KjHQ7hovRXwuUjGQw_Ka_DGnn8r8?usp=sharing',
+  windows:     'https://drive.google.com/drive/folders/1K-30KjHQ7hovRXwuUjGQw_Ka_DGnn8r8?usp=sharing',
+};
---
+++ version.js
@@ -0,0 +1,1 @@
+const APP_VERSION = '2.0.1';
