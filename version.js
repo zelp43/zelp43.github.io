a942f64fcbc4a52096f8e1a75c4f72dc053bcf85
zelp43 <zelp43@gmail.com>
2026-06-26 17:00:11 -0400

    Add files via upload

--- version.js
+++ version.js
@@ -1,1 +1,7 @@
-const APP_VERSION = '2.0.1';
+const APP_VERSION = '2.0.3';
+
+const PRICES = {
+  basic:   { intro: '$29.99',  regular: '$39.99',  orig: '$49.99' },
+  pro:     { intro: '$54.99',  regular: '$64.99',  orig: '$99.99' },
+  proPlus: { intro: '$144.99', regular: '$174.99', orig: '$270'   }
+};
